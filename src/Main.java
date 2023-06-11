21349b967b1340e470a6c9219abcb3ebb6e1ec1b
Aleksandra <alya.karas218@gmail.com>
2023-06-11 18:34:57 +0700

    * дешифровка способом перебора ключей

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
-    static  String alphabet= "абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,””:-!? ";
-    static String textTest = "— сказал он, покачиваясь на стуле и задумчиво глядя себе под ноги. — Сначала вы мне скажете о причинах, побудивших короля французского взять крест, — сказал он, поднимая брови и указывая пальцем на чернильницу, — ";
+    static String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,””:-!? ";
+    static String textTest = "Я решительно замялся, не сказал ни слова больше и " +
+            "чувствовал, что ежели этот злодей-учитель хоть год целый будет молчать" +
+            " и вопросительно смотреть на меня, я все-таки не в состоянии буду произнести" +
+            " более ни одного звука».";
 
     static int step = 5;
 
@@ -12,38 +15,63 @@ public class Main {
         String res = makeCezar(textTest);
         System.out.println(res);
 
+        String resDec = methodBrutforce(res);
+        //System.out.println(resDec);
+
     }
 
     private static String makeCezar(String text) {
         String result = "";
 
-
         for (int i = 0; i < text.length(); i++) {
             String letter = String.valueOf(text.charAt(i));
-
             result = result + convertLetter(letter);
-
         }
-
         return result;
     }
 
-    static String convertLetter(String letter)
-    {
+    static String convertLetter(String letter) {
         String converted = letter;
-
         int index = alphabet.indexOf(letter.toLowerCase());
 
-        if (index >= 0)
-        {
-            index=index+step;
+        if (index >= 0) {
+            index = index + step;
             if (index > alphabet.length()) {
                 index = index - alphabet.length();
             }
-
             converted = String.valueOf(alphabet.charAt(index));
         }
+        return converted;
+    }
+    private static String methodBrutforce(String encrypted) {
+        String resultat = "";
 
+        int stepb = 1;
+        while (stepb <= alphabet.length()) {
+            String res = "";
+            for (int i = 0; i < encrypted.length(); i++) {
+                String letter = String.valueOf(encrypted.charAt(i));
+                res = res + decoding(letter, stepb);
+            }
+
+
+            resultat = res + "\n\n";
+            System.out.println(stepb + " " + res);
+            stepb++;
+        }
+        return resultat;
+    }
+    static String decoding(String letter, int stepl) {
+        String converted = letter;
+        int index = alphabet.indexOf(letter);
+
+        if (index >= 0) {
+            index = index - stepl;
+            if (index < 0) {
+                index = index + alphabet.length();
+            }
+            converted = String.valueOf(alphabet.charAt(index));
+        }
         return converted;
     }
 }
